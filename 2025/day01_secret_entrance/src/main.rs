de594165d467bfb780ffcaf919bcb54bc9f65ede
Seefrass <se.mhfr@proton.me>
2025-12-01 17:22:19 +0100

    2025 day 01 part 2

--- 2025/day01_secret_entrance/src/main.rs
+++ 2025/day01_secret_entrance/src/main.rs
@@ -14,25 +14,47 @@ fn main() {
         .read_to_string(&mut input)
         .expect("could not read from file");
 
-    let mut input = input.trim();
+    let input = input.trim();
 
     //--- Actual Task starts here ---//
 
-    let mut result: u16 = 0;
+    let mut result1: u16 = 0;
+    let mut result2: u16 = 0;
 
     input
         .split("\n")
         .map(|l| decode_line(l))
-        .fold(50, |acc, num| {
-            let res = acc + num;
+        .fold(50, |acc, mut num| {
+            while num >= 100 {
+                num -= 100;
+                result2 += 1;
+            }
 
-            if res % 100 == 0 {
-                result += 1
-            };
+            while num <= -100 {
+                num += 100;
+                result2 += 1;
+            }
+
+            let mut res = acc + num; //this could be < 0 or >= 100, acc is assumed to be \in [0, 99]
+
+            if res < 0 {
+                if acc != 0 {
+                    result2 += 1;
+                }
+                res += 100
+            } else if res > 100 {
+                result2 += 1;
+                res -= 100
+            } else if res == 0 || res == 100 {
+                res = 0;
+                result1 += 1;
+                result2 += 1;
+            }
+
             res
         });
 
-    println!("{}", result)
+    println!("{}, {}", result1, result2)
 }
 
 fn decode_line(line: &str) -> i32 {
