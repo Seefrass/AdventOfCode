499caa4764a4c46d8d55bc56cf13c243486b98ac
Seefrass <se.mhfr@proton.me>
2025-12-03 17:03:54 +0100

    improve 2025 day02 part 1 by using a different approach

--- 2025/day02_gift_shop/src/main.rs
+++ 2025/day02_gift_shop/src/main.rs
@@ -1,7 +1,7 @@
 use std::{
+    cmp,
     fs::File,
     io::{BufReader, Read},
-    ops::RangeInclusive,
 };
 
 const FILE_NAME: &str = "input.txt";
@@ -19,8 +19,14 @@ fn main() {
 
     //--- Actual Task starts here ---//
 
-    let result = input.split(",").map(to_range);
-
+    //let result = input.split(",").map(to_range);
+    let result: u64 = input
+        .split(",")
+        .map(|s| s.split_once("-").unwrap())
+        .map(|(s, e)| generate_invalid_ids(s.parse::<u64>().unwrap(), e.parse::<u64>().unwrap()))
+        .flatten()
+        .sum();
+    /*
     let result1: u64 = result
         .clone()
         .map(|i| find_invalid_ids(i, is_invalid_id))
@@ -31,11 +37,57 @@ fn main() {
         .map(|i| find_invalid_ids(i, is_invalid_id_part_two))
         .flatten()
         .sum();
+    */
 
-    println!("Sum of invalid IDs: {}", result1);
-    println!("Sum of invalid IDs with new rules: {}", result2);
+    println!("Sum of invalid IDs: {}", result);
+    //println!("Sum of invalid IDs with new rules: {}", result2);
 }
 
+fn generate_invalid_ids(range_start: u64, range_end: u64) -> Vec<u64> {
+    let mut result = Vec::new();
+
+    for length in range_start.to_string().len()..=range_end.to_string().len() {
+        if length % 2 == 1 {
+            continue;
+        }
+
+        // 1111
+        let start_num = cmp::max(range_start, min(length));
+        let end_num = cmp::min(range_end, max(length));
+
+        // "1111"
+        let start_str = start_num.to_string();
+        let end_str = end_num.to_string();
+
+        // "11"
+        let start_half_num = start_str.split_at(length / 2).0.parse::<u64>().unwrap();
+        let end_half_num = end_str.split_at(length / 2).0.parse::<u64>().unwrap();
+
+        for i in start_half_num..=end_half_num {
+            let invalid_id = i * 10u64.pow(length as u32 / 2) + i;
+            if invalid_id < range_start || invalid_id > range_end {
+                continue;
+            }
+            result.push(invalid_id);
+        }
+    }
+
+    result
+}
+
+fn min(length: usize) -> u64 {
+    10u64.pow((length - 1) as u32)
+}
+
+fn max(length: usize) -> u64 {
+    let mut result = 9;
+    for _ in 0..length - 1 {
+        result = result * 10 + 9;
+    }
+    result
+}
+
+/*
 fn to_range(range_str: &str) -> RangeInclusive<u64> {
     let (start, end) = range_str.split_once("-").unwrap();
     let (start, end) = (start.parse::<u64>().unwrap(), end.parse::<u64>().unwrap());
@@ -109,3 +161,4 @@ fn is_feasibly_invalid(id_str: &String) -> bool {
     });
     !buckets.iter().any(|&d| d == 1)
 }
+*/
