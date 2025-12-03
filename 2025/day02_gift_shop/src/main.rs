0349ad7b416fc432d0f47e4e6dddababacc6961d
Seefrass <se.mhfr@proton.me>
2025-12-03 17:36:19 +0100

    improve 2025 day02 part 2

--- 2025/day02_gift_shop/src/main.rs
+++ 2025/day02_gift_shop/src/main.rs
@@ -19,58 +19,63 @@ fn main() {
 
     //--- Actual Task starts here ---//
 
-    //let result = input.split(",").map(to_range);
-    let result: u64 = input
+    let result1: u64 = input
         .split(",")
         .map(|s| s.split_once("-").unwrap())
-        .map(|(s, e)| generate_invalid_ids(s.parse::<u64>().unwrap(), e.parse::<u64>().unwrap()))
-        .flatten()
-        .sum();
-    /*
-    let result1: u64 = result
-        .clone()
-        .map(|i| find_invalid_ids(i, is_invalid_id))
+        .map(|(s, e)| {
+            generate_invalid_ids(s.parse::<u64>().unwrap(), e.parse::<u64>().unwrap(), false)
+        })
         .flatten()
         .sum();
 
-    let result2: u64 = result
-        .map(|i| find_invalid_ids(i, is_invalid_id_part_two))
+    let result2: u64 = input
+        .split(",")
+        .map(|s| s.split_once("-").unwrap())
+        .map(|(s, e)| {
+            generate_invalid_ids(s.parse::<u64>().unwrap(), e.parse::<u64>().unwrap(), true)
+        })
         .flatten()
         .sum();
-    */
 
-    println!("Sum of invalid IDs: {}", result);
-    //println!("Sum of invalid IDs with new rules: {}", result2);
+    println!("Sum of invalid IDs: {}", result1);
+    println!("Sum of invalid IDs with new rules: {}", result2);
 }
 
-fn generate_invalid_ids(range_start: u64, range_end: u64) -> Vec<u64> {
+fn generate_invalid_ids(range_start: u64, range_end: u64, part_two: bool) -> Vec<u64> {
     let mut result = Vec::new();
 
     for length in range_start.to_string().len()..=range_end.to_string().len() {
-        if length % 2 == 1 {
-            continue;
-        }
+        let divisors = if part_two {
+            get_unique_divisors(length)
+        } else {
+            if length % 2 == 1 { vec![] } else { vec![2] }
+        };
 
-        // 1111
-        let start_num = cmp::max(range_start, min(length));
-        let end_num = cmp::min(range_end, max(length));
+        for d in divisors {
+            // 1111
+            let start_num = cmp::max(range_start, min(length));
+            let end_num = cmp::min(range_end, max(length));
 
-        // "1111"
-        let start_str = start_num.to_string();
-        let end_str = end_num.to_string();
+            // "1111"
+            let start_str = start_num.to_string();
+            let end_str = end_num.to_string();
 
-        // "11"
-        let start_half_num = start_str.split_at(length / 2).0.parse::<u64>().unwrap();
-        let end_half_num = end_str.split_at(length / 2).0.parse::<u64>().unwrap();
+            // 11
+            let start_frac_num = start_str.split_at(length / d).0.parse::<u64>().unwrap();
+            let end_frac_num = end_str.split_at(length / d).0.parse::<u64>().unwrap();
 
-        for i in start_half_num..=end_half_num {
-            let invalid_id = i * 10u64.pow(length as u32 / 2) + i;
-            if invalid_id < range_start || invalid_id > range_end {
-                continue;
+            for i in start_frac_num..=end_frac_num {
+                let invalid_id = i.to_string().repeat(d).parse::<u64>().unwrap();
+                if invalid_id < range_start || invalid_id > range_end {
+                    continue;
+                }
+                result.push(invalid_id);
             }
-            result.push(invalid_id);
         }
     }
+
+    result.sort();
+    result.dedup();
 
     result
 }
@@ -87,60 +92,6 @@ fn max(length: usize) -> u64 {
     result
 }
 
-/*
-fn to_range(range_str: &str) -> RangeInclusive<u64> {
-    let (start, end) = range_str.split_once("-").unwrap();
-    let (start, end) = (start.parse::<u64>().unwrap(), end.parse::<u64>().unwrap());
-    start..=end
-}
-
-fn find_invalid_ids<F: Fn(u64) -> bool>(range: RangeInclusive<u64>, is_invalid_id: F) -> Vec<u64> {
-    let mut result = Vec::new();
-
-    for id in range {
-        if is_invalid_id(id) {
-            result.push(id);
-        }
-    }
-
-    result
-}
-
-fn is_invalid_id(id: u64) -> bool {
-    let id_str = id.to_string();
-    if id_str.len() % 2 == 1 {
-        return false;
-    }
-    let (id_str1, id_str2) = id_str.split_at(id_str.len() / 2);
-    if id_str1 == id_str2 {
-        return true;
-    }
-    return false;
-}
-
-fn is_invalid_id_part_two(id: u64) -> bool {
-    let id_str = id.to_string();
-
-    if !is_feasibly_invalid(&id_str) {
-        return false;
-    }
-
-    let divisors = get_unique_divisors(id_str.len());
-    for d in divisors {
-        let chunks = id_str
-            .as_bytes()
-            .chunks(id_str.len() / d)
-            .map(str::from_utf8)
-            .collect::<Result<Vec<&str>, _>>()
-            .unwrap();
-        let first = chunks.first().unwrap();
-        if chunks.iter().all(|c| c == first) {
-            return true;
-        }
-    }
-    false
-}
-
 fn get_unique_divisors(n: usize) -> Vec<usize> {
     let mut result = Vec::new();
 
@@ -152,13 +103,3 @@ fn get_unique_divisors(n: usize) -> Vec<usize> {
 
     result
 }
-
-fn is_feasibly_invalid(id_str: &String) -> bool {
-    let mut buckets = vec![0; 10];
-    id_str.chars().for_each(|c| {
-        let idx = c.to_digit(10).unwrap();
-        buckets[idx as usize] += 1;
-    });
-    !buckets.iter().any(|&d| d == 1)
-}
-*/
