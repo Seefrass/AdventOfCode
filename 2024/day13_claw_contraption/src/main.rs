f7e542572673bb80dfe2e315f7b1afe2aa35ea2e
Seefrass <se.mhfr@proton.me>
2026-09-21 14:07:33 +0200

    solve 2024 day13 part2

--- 2024/day13_claw_contraption/src/main.rs
+++ 2024/day13_claw_contraption/src/main.rs
@@ -43,12 +43,12 @@ fn main() {
 
 #[derive(Debug)]
 struct ClawMachine {
-    x_a: u64,
-    y_a: u64,
-    x_b: u64,
-    y_b: u64,
-    x_dest: u64,
-    y_dest: u64,
+    x_a: i64,
+    y_a: i64,
+    x_b: i64,
+    y_b: i64,
+    x_dest: i64,
+    y_dest: i64,
 }
 
 impl ClawMachine {
@@ -73,26 +73,41 @@ impl ClawMachine {
             None
         }
     }
+    //Probem: microlp does not support variable sizes greater than i32:MAX which
+    // is definitely required here!
+    //
+    // ---
+    //
+    // "it is tempting, if the only tool you have is a hammer, to treat everything
+    // as if it were a nail." - Abraham Maslow
+    //
+    // after solving Year 2025 day 10 using linear programming i was amazed, how
+    // elegant a lp solver solution can be. So of course when hearing the terms
+    // "minimum" and other constraints I didn't even think about the fact that this
+    // is a linear problem with 2 terms and 2 variables and thus solvable (with
+    // exactly one solution) by hand...
+    // after some simple linear transformations I made on a piece of paper the solution
+    // is stupidly simple... (and also faster)
+    // still leaving part 1 in though as a lesson, also it is easier to use linear
+    // programming interfaces like good_lp and rooc instead of microlp directy.
     fn solve_adjusted(&self) -> Option<u64> {
-        let mut problem = Problem::new(microlp::OptimizationDirection::Minimize);
-        let a = problem.add_integer_var(3.0, (0, i32::MAX));
-        let b = problem.add_integer_var(1.0, (0, i32::MAX));
-        problem.add_constraint(
-            &[(a, self.x_a as f64), (b, self.x_b as f64)],
-            microlp::ComparisonOp::Eq,
-            (self.x_dest + 10000000000000) as f64,
-        );
-        problem.add_constraint(
-            &[(a, self.y_a as f64), (b, self.y_b as f64)],
-            microlp::ComparisonOp::Eq,
-            (self.y_dest + 10000000000000) as f64,
-        );
-        if let Ok(solution) = problem.solve() {
-            let solution = solution.into_solution().unwrap();
-            Some(solution.objective() as u64)
-        } else {
-            None
+        let b_num =
+            (self.y_dest + 10000000000000) * self.x_a - (self.x_dest + 10000000000000) * self.y_a;
+        let b_denom = self.y_b * self.x_a - self.y_a * self.x_b;
+        if b_num % b_denom != 0 {
+            return None;
         }
+        let b = b_num / b_denom;
+        let a_num = (self.x_dest + 10000000000000) - self.x_b * b;
+        let a_denom = self.x_a;
+        if a_num % a_denom != 0 {
+            return None;
+        }
+        let a = a_num / a_denom;
+        if a < 0 || b < 0 {
+            return None;
+        }
+        Some((3 * a + b) as u64)
     }
 }
 
@@ -106,12 +121,12 @@ impl TryFrom<&str> for ClawMachine {
             // println!("{:?}", c);
             let (_, [x_a, y_a, x_b, y_b, x_dest, y_dest]) = c.extract();
             Ok(ClawMachine {
-                x_a: x_a.parse::<u64>().unwrap(),
-                y_a: y_a.parse::<u64>().unwrap(),
-                x_b: x_b.parse::<u64>().unwrap(),
-                y_b: y_b.parse::<u64>().unwrap(),
-                x_dest: x_dest.parse::<u64>().unwrap(),
-                y_dest: y_dest.parse::<u64>().unwrap(),
+                x_a: x_a.parse::<i64>().unwrap(),
+                y_a: y_a.parse::<i64>().unwrap(),
+                x_b: x_b.parse::<i64>().unwrap(),
+                y_b: y_b.parse::<i64>().unwrap(),
+                x_dest: x_dest.parse::<i64>().unwrap(),
+                y_dest: y_dest.parse::<i64>().unwrap(),
             })
         } else {
             Err(())
